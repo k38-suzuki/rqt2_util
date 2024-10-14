ae7a8983a8d010ac945ebab8914d8ad723f10a59
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2024-10-15 08:40:17 +0900

    Fix setters & getters

--- include/rqt2_util/json_archive.hpp
+++ include/rqt2_util/json_archive.hpp
@@ -21,32 +21,32 @@ public:
     virtual void read(const QJsonObject& json) = 0;
     virtual void write(QJsonObject& json) = 0;
 
-    void set(QJsonObject& json, const QString& key, const int& value);
-    void set(QJsonObject& json, const QString& key, const double& value);
-    void set(QJsonObject& json, const QString& key, const bool& value);
-    void set(QJsonObject& json, const QString& key, const QString& value);
-
-    int get(const QJsonObject& json, const QString& key, const int& init) const;
-    double get(const QJsonObject& json, const QString& key, const double& init) const;
-    bool get(const QJsonObject& json, const QString& key, const bool& init) const;
-    QString get(const QJsonObject& json, const QString& key, const char* init) const;
-
-    void set(QJsonObject& json, const QString& key, QVector<int>& vector);
-    void set(QJsonObject& json, const QString& key, QVector<double>& vector);
-    void set(QJsonObject& json, const QString& key, QVector<bool>& vector);
-    void set(QJsonObject& json, const QString& key, QVector<QString>& vector);
-
-    bool get(const QJsonObject& json, const QString& key, QVector<int>& vector);
-    bool get(const QJsonObject& json, const QString& key, QVector<double>& vector);
-    bool get(const QJsonObject& json, const QString& key, QVector<bool>& vector);
-    bool get(const QJsonObject& json, const QString& key, QVector<QString>& vector);
-
 private:
     bool loadFile(const QString& fileName);
     bool saveFile(const QString& fileName);
 
     QString directory_;
 };
+
+void set(QJsonObject& json, const QString& key, const int& value);
+void set(QJsonObject& json, const QString& key, const double& value);
+void set(QJsonObject& json, const QString& key, const bool& value);
+void set(QJsonObject& json, const QString& key, const QString& value);
+
+int get(const QJsonObject& json, const QString& key, const int& init);
+double get(const QJsonObject& json, const QString& key, const double& init);
+bool get(const QJsonObject& json, const QString& key, const bool& init);
+QString get(const QJsonObject& json, const QString& key, const char* init);
+
+void set(QJsonObject& json, const QString& key, QVector<int>& vector);
+void set(QJsonObject& json, const QString& key, QVector<double>& vector);
+void set(QJsonObject& json, const QString& key, QVector<bool>& vector);
+void set(QJsonObject& json, const QString& key, QVector<QString>& vector);
+
+bool get(const QJsonObject& json, const QString& key, QVector<int>& vector);
+bool get(const QJsonObject& json, const QString& key, QVector<double>& vector);
+bool get(const QJsonObject& json, const QString& key, QVector<bool>& vector);
+bool get(const QJsonObject& json, const QString& key, QVector<QString>& vector);
 
 }  // namespace rqt2_util
 
--- src/json_archive.cpp
+++ src/json_archive.cpp
@@ -58,150 +58,6 @@ bool JsonArchive::save(QWidget* parent)
     return true;
 }
 
-void JsonArchive::set(QJsonObject& json, const QString& key, const int& value)
-{
-    json[key] = value;
-}
-
-void JsonArchive::set(QJsonObject& json, const QString& key, const double& value)
-{
-    json[key] = value;
-}
-
-void JsonArchive::set(QJsonObject& json, const QString& key, const bool& value)
-{
-    json[key] = value;
-}
-
-void JsonArchive::set(QJsonObject& json, const QString& key, const QString& value)
-{
-    json[key] = value;
-}
-
-int JsonArchive::get(const QJsonObject& json, const QString& key, const int& init) const
-{
-    int ret = init;
-    if(const QJsonValue v = json[key]; v.isDouble()) {
-        ret = v.toInt();
-    }
-    return ret;
-}
-
-double JsonArchive::get(const QJsonObject& json, const QString& key, const double& init) const
-{
-    double ret = init;
-    if(const QJsonValue v = json[key]; v.isDouble()) {
-        ret = v.toDouble();
-    }
-    return ret;
-}
-
-bool JsonArchive::get(const QJsonObject& json, const QString& key, const bool& init) const
-{
-    bool ret = init;
-    if(const QJsonValue v = json[key]; v.isBool()) {
-        ret = v.toBool();
-    }
-    return ret;
-}
-
-QString JsonArchive::get(const QJsonObject& json, const QString& key, const char* init) const
-{
-    QString ret = init;
-    if(const QJsonValue v = json[key]; v.isString()) {
-        ret = v.toString();
-    }
-    return ret;
-}
-
-void JsonArchive::set(QJsonObject& json, const QString& key, QVector<int>& vector)
-{
-    QJsonArray array;
-    for(const auto& value : vector) {
-        array.append(value);
-    }
-    json[key] = array;
-}
-
-void JsonArchive::set(QJsonObject& json, const QString& key, QVector<double>& vector)
-{
-    QJsonArray array;
-    for(const auto& value : vector) {
-        array.append(value);
-    }
-    json[key] = array;
-}
-
-void JsonArchive::set(QJsonObject& json, const QString& key, QVector<bool>& vector)
-{
-    QJsonArray array;
-    for(const auto& value : vector) {
-        array.append(value);
-    }
-    json[key] = array;
-}
-
-void JsonArchive::set(QJsonObject& json, const QString& key, QVector<QString>& vector)
-{
-    QJsonArray array;
-    for(const auto& value : vector) {
-        array.append(value);
-    }
-    json[key] = array;
-}
-
-bool JsonArchive::get(const QJsonObject& json, const QString& key, QVector<int>& vector)
-{
-    vector.clear();
-    if(const QJsonValue v = json[key]; v.isArray()) {
-        const QJsonArray array = v.toArray();
-        for(const auto& value : array) {
-            vector.push_back(value.toInt());
-        }
-        return true;
-    }
-    return false;
-}
-
-bool JsonArchive::get(const QJsonObject& json, const QString& key, QVector<double>& vector)
-{
-    vector.clear();
-    if(const QJsonValue v = json[key]; v.isArray()) {
-        const QJsonArray array = v.toArray();
-        for(const auto& value : array) {
-            vector.push_back(value.toDouble());
-        }
-        return true;
-    }
-    return false;
-}
-
-bool JsonArchive::get(const QJsonObject& json, const QString& key, QVector<bool>& vector)
-{
-    vector.clear();
-    if(const QJsonValue v = json[key]; v.isArray()) {
-        const QJsonArray array = v.toArray();
-        for(const auto& value : array) {
-            vector.push_back(value.toBool());
-        }
-        return true;
-    }
-    return false;
-}
-
-bool JsonArchive::get(const QJsonObject& json, const QString& key, QVector<QString>& vector)
-{
-    vector.clear();
-    if(const QJsonValue v = json[key]; v.isArray()) {
-        const QJsonArray array = v.toArray();
-        for(const auto& value : array) {
-            vector.push_back(value.toString());
-        }
-        return true;
-    }
-    return false;
-}
-
 bool JsonArchive::loadFile(const QString& fileName)
 {
     QFile loadFile(fileName);
@@ -234,6 +90,150 @@ bool JsonArchive::saveFile(const QString& fileName)
     saveFile.write(QJsonDocument(jsonObject).toJson());
 
     return true;
+}
+
+void set(QJsonObject& json, const QString& key, const int& value)
+{
+    json[key] = value;
+}
+
+void set(QJsonObject& json, const QString& key, const double& value)
+{
+    json[key] = value;
+}
+
+void set(QJsonObject& json, const QString& key, const bool& value)
+{
+    json[key] = value;
+}
+
+void set(QJsonObject& json, const QString& key, const QString& value)
+{
+    json[key] = value;
+}
+
+int get(const QJsonObject& json, const QString& key, const int& init)
+{
+    int ret = init;
+    if(const QJsonValue v = json[key]; v.isDouble()) {
+        ret = v.toInt();
+    }
+    return ret;
+}
+
+double get(const QJsonObject& json, const QString& key, const double& init)
+{
+    double ret = init;
+    if(const QJsonValue v = json[key]; v.isDouble()) {
+        ret = v.toDouble();
+    }
+    return ret;
+}
+
+bool get(const QJsonObject& json, const QString& key, const bool& init)
+{
+    bool ret = init;
+    if(const QJsonValue v = json[key]; v.isBool()) {
+        ret = v.toBool();
+    }
+    return ret;
+}
+
+QString get(const QJsonObject& json, const QString& key, const char* init)
+{
+    QString ret = init;
+    if(const QJsonValue v = json[key]; v.isString()) {
+        ret = v.toString();
+    }
+    return ret;
+}
+
+void set(QJsonObject& json, const QString& key, QVector<int>& vector)
+{
+    QJsonArray array;
+    for(const auto& value : vector) {
+        array.append(value);
+    }
+    json[key] = array;
+}
+
+void set(QJsonObject& json, const QString& key, QVector<double>& vector)
+{
+    QJsonArray array;
+    for(const auto& value : vector) {
+        array.append(value);
+    }
+    json[key] = array;
+}
+
+void set(QJsonObject& json, const QString& key, QVector<bool>& vector)
+{
+    QJsonArray array;
+    for(const auto& value : vector) {
+        array.append(value);
+    }
+    json[key] = array;
+}
+
+void set(QJsonObject& json, const QString& key, QVector<QString>& vector)
+{
+    QJsonArray array;
+    for(const auto& value : vector) {
+        array.append(value);
+    }
+    json[key] = array;
+}
+
+bool get(const QJsonObject& json, const QString& key, QVector<int>& vector)
+{
+    vector.clear();
+    if(const QJsonValue v = json[key]; v.isArray()) {
+        const QJsonArray array = v.toArray();
+        for(const auto& value : array) {
+            vector.push_back(value.toInt());
+        }
+        return true;
+    }
+    return false;
+}
+
+bool get(const QJsonObject& json, const QString& key, QVector<double>& vector)
+{
+    vector.clear();
+    if(const QJsonValue v = json[key]; v.isArray()) {
+        const QJsonArray array = v.toArray();
+        for(const auto& value : array) {
+            vector.push_back(value.toDouble());
+        }
+        return true;
+    }
+    return false;
+}
+
+bool get(const QJsonObject& json, const QString& key, QVector<bool>& vector)
+{
+    vector.clear();
+    if(const QJsonValue v = json[key]; v.isArray()) {
+        const QJsonArray array = v.toArray();
+        for(const auto& value : array) {
+            vector.push_back(value.toBool());
+        }
+        return true;
+    }
+    return false;
+}
+
+bool get(const QJsonObject& json, const QString& key, QVector<QString>& vector)
+{
+    vector.clear();
+    if(const QJsonValue v = json[key]; v.isArray()) {
+        const QJsonArray array = v.toArray();
+        for(const auto& value : array) {
+            vector.push_back(value.toString());
+        }
+        return true;
+    }
+    return false;
 }
 
 }  // namespace rqt2_util
